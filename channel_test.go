4ebeb2d4bbe07797fb287a0fec39d4c7d0332db1
fahmiaz411 <fahmiaziz884@gmail.com>
2022-03-08 20:57:48 +0700

    buffer, in out, select

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package goroutine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -20,7 +21,7 @@ func TestChannel(t *testing.T){
 
 }
 
-func ChanParam (c chan string){
+func ChanParam (c chan <- string){
 	time.Sleep(5 * time.Second)
 	c <- "test param"
 }
@@ -47,4 +48,73 @@ func BenchmarkParam(b *testing.B) {
 
 		fmt.Println(data)
 	}
+}
+
+func OnlyIn(channel chan <- string) {
+	// time.Sleep(time.Second)
+	channel <- "hey"
+}
+
+func OnlyOut(channel chan string){
+	defer close(channel)
+	data := <- channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel (t *testing.T){
+	channel := make(chan string)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+}
+
+func TestBufferChannel (t *testing.T){
+	channel := make(chan string, 3)
+	
+	go func(){
+		defer close(channel)
+		for i:= 1; i<=10;i++{
+			channel <- "data " + strconv.Itoa(i)
+		}
+	}()
+
+	for data := range channel{
+		fmt.Println(data)
+	}
+	
+	fmt.Println("end")
+}
+
+func TestSelectChannel (t *testing.T){
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer func () {
+		close(channel1)
+		close(channel2)
+	}()
+
+	go OnlyIn(channel1)
+	go OnlyIn(channel2)
+
+	counter := 0
+
+	for {
+		select {
+			case data:= <- channel1:
+				fmt.Println("data 1", data)
+				counter++
+			case data := <- channel2:
+				fmt.Println("data 2", data)
+				counter++
+			default:
+				fmt.Println("wait data")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+
+
+	fmt.Println("end")
 }
